42b48abdbd4c05fc97c55e0ae6e3eb5ce83f65ef
Andreas Rauch <mail@andreas-rauch.de>
2021-03-01 10:11:52 +0100

    Make rf code an argument

--- rf_receiver.py
+++ rf_receiver.py
@@ -25,7 +25,9 @@ parser.add_argument('--gpio', dest='gpio', type=int, default=27,
 parser.add_argument('--ip_address', dest='ip_address', type=str, required=true,
                     help="IP address of Openhab")
 parser.add_argument('--item', dest='item', type=str, required=true,
-                    help="Item to be switched")					
+                    help="Item to be switched")
+parser.add_argument('--code', dest='code', type=str, required=true,
+                    help="Code to be received")
 args = parser.parse_args()
 
 item_address = "http://" + args.ip_address + ":8080/rest/items/" + args.item
@@ -40,7 +42,7 @@ while True:
         logging.info(str(rfdevice.rx_code) +
                      " [pulselength " + str(rfdevice.rx_pulselength) +
                      ", protocol " + str(rfdevice.rx_proto) + "]")
-		if(str(rfdevice.rx_code) == "15555555"){
+		if(str(rfdevice.rx_code) == args.code){
 			logging.info("Sending RestAPI Post to Openhab")
 			resp = requests.post(item_address, data='ON', headers={'Content-Type': 'text/plain', 'Accept': 'application/json'})
 		}
